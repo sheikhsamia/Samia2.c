97d7dea2f74d50493b52b1cb4207a7f7107019ac
sheikhsamia <sheikhsamia52@gmail.com>
2025-08-19 12:17:35 +0600

    Add files via upload

---
+++ samia2.c
@@ -0,0 +1,15 @@
+    #include<stdio.h>
+    int main() {
+    int x,y;
+    printf("enter the first number: ");
+    scanf("%d",&x);
+    printf("enter the second number: ");
+    scanf("%d",&y);
+    if (x == y){
+    printf("both numbers are equal,\n");
+    }
+    else{ 
+    printf("numbers are not equal,\n");
+    }
+    return 0;
+    }
